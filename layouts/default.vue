0b63c027386a4afc987319bf12f55753ace18f1f
sh1r00 <rasisg@gmail.com>
2020-08-11 10:52:20 -0700

    added nav bar scroll effect

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,13 @@
 <template>
-  <div>
-    <nav class="navbar is-white has-shadow">
+  <div id="topId">
+    <nav  style="margin-bottom: 5em;" :style="{ height: navEffect.navHeight }" class="navbar is-fixed-top is-centered is-white has-shadow">
       <div class="container">
-        <div class="navbar-brand">
-          <router-link to="/" class="navbar-item is-size-3 logo has-text-dark">
+        <button class="nav-logo button is-text is-large">
+          <div class="navbar-brand is-text is-size-3 logo has-text-dark" @click="scrollToTop()">
             The<b>Case</b> Company
-          </router-link>
-        </div>
-        <div class="navbar-menu is-active">
+          </div>
+        </button>
+        <div :style="{ display: `${navEffect.cart}`}" class="nav-cart navbar-menu is-centered is-active">
           <div class="navbar-end">
             <div class="navbar-item">
               <nuxt-link :to="{name: 'cart'}"> Cart ({{ cartTotal }})</nuxt-link>
@@ -16,7 +16,7 @@
         </div>
       </div>
     </nav>
-    <nuxt/>
+    <nuxt />
     <footer class="footer">
       <div class="container">
         <div class="content has-text-centered">
@@ -28,9 +28,42 @@
 </template>
 <script>
 import { mapGetters } from 'vuex'
+import debounce from '~/components/utils/debounce.js'
   export default {
+    data() {
+      return {
+        navEffect: {
+          cart: 'none',
+          navHeight: '6.25em'
+        }
+      }
+    },
     computed: {
       ...mapGetters(['cartTotal'])
+    },
+    mounted() {
+      this.handleDebouncedScroll = debounce(this.handleScroll, 5);
+      window.addEventListener('scroll', this.handleDebouncedScroll)
+    },
+    destroyed() {
+      this.handleDebouncedScroll = debounce(this.handleScroll, 5);
+      window.removeEventListener('scroll', this.handleDebouncedScroll)
+    },
+    methods: {
+      handleScroll(event) {
+        console.log('scrolling ', window.scrollY)
+        if (window.scrollY > 5) {
+          this.navEffect.cart = 'inline'
+          this.navEffect.navHeight = 'auto'
+        } 
+        if (window.scrollY <= 5) {
+          this.navEffect.cart = 'none'
+          this.navEffect.navHeight = '6.25em'
+        }
+      },
+      scrollToTop() {
+        document.getElementById('topId').scrollIntoView({behavior: 'smooth', block: 'start'})
+      }
     }
   }
 </script>
